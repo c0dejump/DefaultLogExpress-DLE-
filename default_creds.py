4318ef4c3591b7c2c68601f5998481ce933bd074
c0dejump <codejumpgame@gmail.com>
2018-10-15 17:18:47 +0200

    Create default_creds.py

---
+++ default_creds.py
@@ -0,0 +1,4 @@
+DEFAULT = {
+          "username": ["both", "admin", "manager", "role1", "root", "role", "tomcat", "superuser","test"],
+          "password": ["Password1", "password1", "password", "admin", "tomcat", "changethis", "manager", "root", "r00t", "toor", "s3cret", "role1", "superuser", "","test"],
+         }
