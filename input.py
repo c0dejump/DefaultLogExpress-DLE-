9d6db7f51f6400e68042f23f41c78e54d7c47165
c0dejump <codejumpgame@gmail.com>
2018-10-15 17:18:23 +0200

    Create input.py

---
+++ input.py
@@ -0,0 +1,4 @@
+INPUT = {
+         "login" : ["login", "username", "user", "user_login", "pseudo", "Pseudo", "utilisateur", "log", "name"],
+	     "passwd" : ["password", "Password", "passwd", "pass", "user_pass", "mpasse", "Mpasse", "pwd"],
+}
