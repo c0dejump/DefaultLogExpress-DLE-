f8900da89c2b99ec0a4fab071229dc4fb1b1e3df
c0dejump <codejumpgame@gmail.com>
2018-11-09 11:53:09 +0100

    Update input.py

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
 INPUT = {
-         "login" : ["login", "username", "user", "user_login", "pseudo", "Pseudo", "utilisateur", "log", "name"],
-	     "passwd" : ["password", "Password", "passwd", "pass", "user_pass", "mpasse", "Mpasse", "pwd"],
+         "login" : ["login", "username", "user", "user_login", "pseudo", "Pseudo", "utilisateur", "log", "name", "pma_username", "txtUser", "login-username", "pom_auth_username"],
+	     "passwd" : ["password", "Password", "passwd", "pass", "user_pass", "mpasse", "Mpasse", "pwd", "pma_password", "txtPassword", "login-password", "pom_auth_password"],
 }
