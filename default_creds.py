6ed93e366be7744b7b7e8f47293e3763aca1691e
c0dejump <codejumpgame@gmail.com>
2018-11-09 11:53:29 +0100

    Update default_creds.py

--- default_creds.py
+++ default_creds.py
@@ -1,4 +1,4 @@
 DEFAULT = {
-          "username": ["both", "admin", "manager", "role1", "root", "role", "tomcat", "superuser","test"],
-          "password": ["Password1", "password1", "password", "admin", "tomcat", "changethis", "manager", "root", "r00t", "toor", "s3cret", "role1", "superuser", "","test"],
+          "username": ["admin", "manager", "both", "role1", "root", "role", "tomcat", "superuser","test","administrator"],
+          "password": ["Password1", "password1", "password", "admin", "tomcat", "changethis", "manager", "root", "r00t", "toor", "s3cret", "role1", "superuser", "test", "koolix", "1234", "admin1234","azertyuiop"],
          }
